b7edfbe065f058859b84bab48cb232087358b1dd
JaredLarios <lar21038@byui.edu>
2025-03-13 22:43:25 -0600

    feat: Add delete functionality for cart items and update cart display

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,26 +1,41 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+function deleteCartContent(event) {
+  const itemId = event.target.getAttribute("data-id");
+  let cartItems = getLocalStorage("so-cart");
+
+  cartItems = cartItems.filter((item) => item.Id !== itemId);
+  setLocalStorage("so-cart", cartItems);
+
+  renderCartContents();
+}
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.querySelectorAll(".cart-card__delete").forEach((button) => {
+    button.addEventListener("click", deleteCartContent);
+  });
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  const newItem = `
+  <li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>  
+    <span class="cart-card__delete" data-id=${item.Id}>X</span>
+  </li>`;
 
   return newItem;
 }
