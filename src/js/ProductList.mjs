e7d3009acf306ed01aa2360e31c83c4bf669828a
JaredLarios <lar21038@byui.edu>
2025-03-19 07:47:02 -0600

    feat: update product data fetching to use dynamic category and server URL

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,7 +3,7 @@ import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product) {
     return `<li class="product-card">
                 <a href="/product_pages/?product=${product.Id}">
-                <img src="${product.Image}" alt="Image of ">
+                <img src="${product.Images.PrimaryLarge}" alt="Image of ${product.Name}">
                 <h2 class="card__brand">${product.Brand.Name}</h2>
                 <h3 class="card__name">${product.Name}</h3>
                 <p class="product-card__price">$${product.FinalPrice}</p>
@@ -18,7 +18,7 @@ export default class ProductList {
         this.listElement = listElement;
     }
     async init(){
-        const myList = await this.dataSource.getData();
+        const myList = await this.dataSource.getData(this.category);
         this.renderList(myList);
     }
     renderList(list){
--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,13 +1,17 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
-import { loadHeaderFooter } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 
-const dataSource = new ProductData("tents");
+loadHeaderFooter();
+
+const category = getParam("category");
+
+const dataSource = new ProductData();
 
 const productList = document.querySelector(".product-list");
-const productListning = new ProductList("Tents", dataSource, productList);
+const productListning = new ProductList(category, dataSource, productList);
 
 productListning.init();
 
-loadHeaderFooter();
+
