b20c0ad18960379fd0d025a33a8bd98e076884c4
JaredLarios <lar21038@byui.edu>
2025-03-18 21:39:33 -0600

    Refactor: header and footer structure; implement dynamic loading of header and footer templates

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const productList = document.querySelector(".product-list");
 const productListning = new ProductList("Tents", dataSource, productList);
 
 productListning.init();
+
+loadHeaderFooter();
