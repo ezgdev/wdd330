eeafd9cff743cddab7938c4696901649bab5c6d7
Cherry-Machado <Cherry5467@gmail.com>
2025-04-05 06:33:43 -0300

    Updating

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -3,81 +3,79 @@ import {
   alertMessage,
   setLocalStorage,
   removeAllAlerts,
-  getLocalStorage,
 } from "./utils.mjs";
 
 import CheckoutProcess from "./CheckoutProcess.mjs";
 
 loadHeaderFooter();
 
-  // Initialize the CheckoutProcess with key 'so-cart' and output selectors
-  const checkout = new CheckoutProcess("so-cart", {
-    subtotal: "#subtotal",
-    shipping: "#shipping",
-    tax: "#tax",
-    total: "#total",
-  });
+// Initialize the CheckoutProcess with key 'so-cart' and output selectors
+const checkout = new CheckoutProcess("so-cart", {
+  subtotal: "#subtotal",
+  shipping: "#shipping",
+  tax: "#tax",
+  total: "#total",
+});
 
-  checkout.init(); // Initialize the process to calculate the item subtotal
+checkout.init(); // Initialize the process to calculate the item subtotal
 
-  // Add an event listener for ZIP code input to trigger the order total calculation
-  document.querySelector("#zip").addEventListener("input", (event) => {
-    const zipCode = event.target.value;
-    // Validate ZIP code length (5 digits)
-    if (zipCode.length === 5) {
-      checkout.calculateOrderTotal();
-    } else {
-      document.querySelector(checkout.outputSelector.shipping).innerText =
-        "0.00";
-      document.querySelector(checkout.outputSelector.tax).innerText = "0.00";
-      document.querySelector(checkout.outputSelector.total).innerText = "0.00";
+// Add an event listener for ZIP code input to trigger the order total calculation
+document.querySelector("#zip").addEventListener("input", (event) => {
+  const zipCode = event.target.value;
+  // Validate ZIP code length (5 digits)
+  if (zipCode.length === 5) {
+    checkout.calculateOrderTotal();
+  } else {
+    document.querySelector(checkout.outputSelector.shipping).innerText = "0.00";
+    document.querySelector(checkout.outputSelector.tax).innerText = "0.00";
+    document.querySelector(checkout.outputSelector.total).innerText = "0.00";
+  }
+});
+
+// Handle form submission for checkout
+document
+  .querySelector("#checkout-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    //Get the form from the event.
+    const myForm = event.target;
+
+    // Check if the form is valid
+    const isFormValid = myForm.checkValidity();
+    myForm.reportValidity(); // Show validation messages
+    if (!isFormValid) {
+      alertMessage("Please fill in all required fields.", true);
+      return;
+    }
+
+    // Validate card number and expiration date manually
+    const cardNumber = myForm.cardNumber.value;
+    const expirationDate = myForm.expiration.value;
+
+    const isCardNumberValid = isValidCardNumber(cardNumber);
+    const isExpirationValid = isValidExpirationDate(expirationDate);
+
+    if (!isCardNumberValid) {
+      alertMessage("Invalid Card Number", true);
+      return;
+    }
+
+    if (!isExpirationValid) {
+      alertMessage("Invalid Expiration Date", true);
+      return;
+    }
+
+    // Show validation messages
+    try {
+      await checkout.checkout(myForm);
+      setLocalStorage("so-cart", []);
+      removeAllAlerts();
+      window.location.href = "/checkout/success.html";
+    } catch (error) {
+      alertMessage("Error when processing the order: " + error.message, true);
     }
   });
-
-  // Handle form submission for checkout
-  document
-    .querySelector("#checkout-form")
-    .addEventListener("submit", async (event) => {
-      event.preventDefault();
-
-      //Get the form from the event.
-      const myForm = event.target;
-
-      // Check if the form is valid
-      const isFormValid = myForm.checkValidity();
-      myForm.reportValidity(); // Show validation messages
-      if (!isFormValid) {
-        alertMessage("Please fill in all required fields.", true);
-        return;
-      }
-
-      // Validate card number and expiration date manually
-      const cardNumber = myForm.cardNumber.value;
-      const expirationDate = myForm.expiration.value;
-
-      const isCardNumberValid = isValidCardNumber(cardNumber);
-      const isExpirationValid = isValidExpirationDate(expirationDate);
-
-      if (!isCardNumberValid) {
-        alertMessage("Invalid Card Number", true);
-        return;
-      }
-
-      if (!isExpirationValid) {
-        alertMessage("Invalid Expiration Date", true);
-        return;
-      }
-
-      // Show validation messages
-      try {
-        await checkout.checkout(myForm);
-        setLocalStorage("so-cart", []);
-        removeAllAlerts();
-        window.location.href = "/checkout/success.html";
-      } catch (error) {
-        alertMessage("Error when processing the order: " + error.message, true);
-      }
-    });
 
 function isValidCardNumber(cardNumber) {
   // Card number validation logic (let 13-19 digits)
