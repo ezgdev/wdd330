d7128c7997f65c7f82b0448dad54ba891865675a
Cherry-Machado <Cherry5467@gmail.com>
2025-04-05 06:04:57 -0300

    Updating

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -9,6 +9,7 @@ import CheckoutProcess from "./CheckoutProcess.mjs";
 
 loadHeaderFooter();
 
+checkout.init(); // Initialize the process to calculate the item subtotal
 // Initialize the CheckoutProcess with key 'so-cart' and output selectors
 const checkout = new CheckoutProcess("so-cart", {
   subtotal: "#subtotal",
@@ -17,7 +18,7 @@ const checkout = new CheckoutProcess("so-cart", {
   total: "#total",
 });
 
-checkout.init(); // Initialize the process to calculate the item subtotal
+
 
 // Add an event listener for ZIP code input to trigger the order total calculation
 document.querySelector("#zip").addEventListener("input", (event) => {
